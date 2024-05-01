dae7ba22bb9fa4c5ac2cb30318c93350affe38aa
Noach <torah_mishna@proton.me>
2024-05-01 15:39:34 -0400

    Update3

---
+++ java-class-work/JavaClasswork/src/main/java/org/example/Homework_303_2_1.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Homework_303_2_1 {
+    public static void main(String[] args) {
+        int var1 = 10;
+        int var2 = 20;
+
+        int sum = var2 + var1;
+        System.out.println(sum);
+    }
+}
