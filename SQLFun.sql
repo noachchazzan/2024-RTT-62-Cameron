217bd663b671ada980ecb53374d881b7bf419be2
noachchazzan <torah_mishna@proton.me>
2024-05-29 23:07:29 -0400

    GLAB304_4_1wPA304_4_1

--- SQLFun.sql
+++ SQLFun.sql
@@ -15,9 +15,66 @@ FROM orders ORDER BY status ASC;
 /* Display all fields from the payments table for payments made on or after January 1, 2005. The output should be sorted by the payment date from highest to lowest.*/
 SELECT * FROM payments WHERE paymentDate >= '2005-01-01' ORDER BY paymentDate DESC;
 /* Display the last Name, first Name, email address, and job title of all employees working out of the San Francisco office. The output should be sorted by “Last Name.”*/
-SELECT * FROM employees e 
-JOIN offices o ON e.officeCode = o.officeCode
-WHERE o.city = "San Francisco";
+/* Display the last name, first name, email address, and job title of all employees working out of the San Francisco office. The output should be sorted by “Last Name.” */
+
+SELECT 
+    e.lastName AS 'Last Name', 
+    e.firstName AS 'First Name', 
+    e.email AS 'Email Address', 
+    e.jobTitle AS 'Job Title'
+FROM 
+    employees e
+JOIN 
+    offices o ON e.officeCode = o.officeCode
+WHERE 
+    o.city = 'San Francisco'
+ORDER BY 
+    e.lastName;
+
 /* Display the name, product line, scale, and vendor of all of the car products – both classic and vintage. */
 /*The output should display all vintage cars first (sorted alphabetically by name), and all classic cars last (also sorted alphabetically by name). */
+/* Display the name, product line, scale, and vendor of all of the car products – both classic and vintage. */
+/* The output should display all vintage cars first (sorted alphabetically by name), and all classic cars last (also sorted alphabetically by name). */
+
 SELECT 
+    productName AS 'Name', 
+    productLine AS 'Product Line', 
+    productScale AS 'Scale', 
+    productVendor AS 'Vendor'
+FROM 
+    products
+WHERE 
+    productLine = 'Vintage Cars'
+ORDER BY 
+    productName ASC
+
+UNION ALL
+
+SELECT 
+    productName AS 'Name', 
+    productLine AS 'Product Line', 
+    productScale AS 'Scale', 
+    productVendor AS 'Vendor'
+FROM 
+    products
+WHERE 
+    productLine = 'Classic Cars'
+ORDER BY 
+    productName ASC;
+
+
+SELECT    orderNumber, orderlinenumber, quantityOrdered * priceEach
+FROM    orderdetails
+ORDER BY   quantityOrdered * priceEach DESC;
+
+SELECT 
+    orderNumber,
+    orderLineNumber,
+    quantityOrdered * priceEach AS subtotal
+FROM    orderdetails
+ORDER BY subtotal DESC;
+
+SELECT    firstName, lastName, reportsTo
+FROM    employees
+ORDER BY reportsTo DESC;
+
